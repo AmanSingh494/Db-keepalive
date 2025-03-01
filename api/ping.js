1c6701e9220426ac8109071f208955313ff08d89
AmanSingh494 <amanforwork1@gmail.com>
2025-03-01 23:39:15 +0530

    Added all projects

--- api/ping.js
+++ api/ping.js
@@ -1,19 +1,32 @@
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_KEY = process.env.SUPABASE_KEY
 export default async function handler(req, res) {
+  const urls = [
+    process.env.SUPABASE_URL_ORCHARD_EYES,
+    process.env.SUPABASE_URL_SEED_SOCIETY,
+    process.env.SUPABASE_URL_AUTODOC
+  ]
+  const keys = [
+    process.env.SUPABASE_KEY_ORCHARD_EYES,
+    process.env.SUPABASE_KEY_SEED_SOCIETY,
+    process.env.SUPABASE_KEY_AUTODOC
+  ]
   try {
-    const response = await fetch(`${SUPABASE_URL}/rest/v1/`, {
-      headers: {
-        apikey: SUPABASE_KEY
-      }
-    })
+    const results = await Promise.all(
+      urls.map(async (url, index) => {
+        const response = await fetch(`${url}/rest/v1/`, {
+          method: 'GET',
+          headers: {
+            apikey: keys[index]
+          }
+        })
 
-    if (response.ok) {
-      res.status(200).json({ message: 'Supabase is active' })
-    } else {
-      res.status(500).json({ error: 'Failed to ping Supabase' })
-    }
+        return response.ok
+          ? `Project ${index + 1} alive`
+          : `Project ${index + 1} failed`
+      })
+    )
+
+    res.status(200).json({ message: 'Pings sent', results })
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: 'Ping failed', details: error.message })
   }
 }
